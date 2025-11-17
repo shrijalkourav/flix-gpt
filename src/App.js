2cfbb5e48f43e3e931662603b585fdc956227605
Shrajal Kaurav <96686794+Shrajalkaurav@users.noreply.github.com>
2025-11-17 16:03:48 +0530

    tailwind fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Namaste Everyone, Lets build Flix-GPT</div>;
+  return (
+    <div className="text-center text-3xl font-bold text-red-500">
+      Namaste Everyone, Lets build Flix-GPT
+    </div>
+  );
 };
 
 export default App;
